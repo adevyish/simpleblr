bda789e18363e28cf1d1b49f669c58d6ec5f98e1
adevyish <adevyish@gmail.com>
2022-03-10 01:58:32 -0800

    Add v1
    - Add making NPF photos on own row higher res
    - Add removing leading breaks in reblogs
    - Add removing empty paragraphs in reblogs
    - Add laying out photosets

--- README.md
+++ README.md
@@ -1,1 +1,67 @@
 # simpleblr
+
+This is a lightweight set of Javascript utilities for Tumblr themes. It does not rely on any external libraries like jQuery.
+
+Endless scrolling and layout resizing are not supported out of the box, but both should be easy to hook up.
+
+## Setup
+
+Add after the `<style> tag:
+	
+```
+<link rel="stylesheet" href="/path/to/simpleblr.css" />
+```
+
+Add before the `</body>` tag:
+
+```
+<script src="/path/to/simpleblr.js"></script>
+<script>
+	Simpleblr.run()
+</script>
+```
+
+You can also only run individual functions if you want to.
+
+## NPF Posts
+
+NPF reblogs are supported with `data-reblog-item-body`.
+
+For example:
+
+```
+{block:RebloggedFrom}
+	<div>
+		{block:Reblogs}
+			<section>
+				<div>{Username}</div>
+				<div data-reblog-item-body>
+					{Body}
+				</div>
+			</section>
+		{/block:Reblogs}
+	</div>
+{/block:RebloggedFrom}
+```
+
+## Photosets
+
+Custom photosets are supported with `data-photoset-layout` and `data-photoset-item`.
+
+For example:
+
+```
+{block:Photoset}
+	<div data-photoset-layout="{PhotosetLayout}">
+		{block:Photos}
+			<span data-photoset-item>
+				{block:LinkURL}<a href="{LinkURL}">{/block:LinkURL}
+				{block:HighRes}<a href="{PhotoURL-HighRes}">{/block:HighRes}
+					<img src="{PhotoURL-1280}" alt="{PhotoAlt}">
+				{block:HighRes}</a>{/block:HighRes}
+				{block:LinkURL}</a>{/block:LinkURL}
+			</span>
+		{/block:Photos}
+	</div>
+{/block:Photoset}
+```
---
+++ simpleblr.js
@@ -0,0 +1,88 @@
+var Simpleblr = Simpleblr || {}
+
+Simpleblr.MAX_ROW_HEIGHT = Simpleblr.MAX_ROW_HEIGHT || 1280
+Simpleblr.PHOTOSET_SPACING = Simpleblr.PHOTOSET_SPACING || 4
+
+/* Make NPF photos that are on their own row higher res */
+Simpleblr.loadHighResNPFPhotos = function () {
+    document.querySelectorAll('a[data-big-photo]').forEach(function(link) {
+        if (link.closest('.npf_col') === null) {
+            var photo = link.querySelector('img')
+            photo.src = link.dataset.bigPhoto
+        }
+    })
+}
+
+/* Remove <br>s at the start of paragraphs in reblogged posts */
+Simpleblr.removeReblogLeadingBreaks = function () {
+    document.querySelectorAll('[data-reblog-item-body] p > br').forEach(function (br) {
+        if (br.parentElement.firstChild === br) {
+            br.remove()
+        }
+    })
+}
+
+/* Remove empty <p>s from reblogged posts. Must be run after removeReblogLeadingBreaks. */
+Simpleblr.removeReblogEmptyParagraphs = function () {
+    document.querySelectorAll('[data-reblog-item-body] p:empty').forEach(function (p) {
+        p.remove()
+    })
+}
+
+/* Layout photosets */
+Simpleblr.layoutPhotosets = function() {
+    var query = '[data-photoset-layout]:not([data-photoset-created])'
+    document.querySelectorAll(query).forEach(function (photoset) {
+        var items = photoset.querySelectorAll('[data-photoset-item]')
+        var itemIndex = 0
+        var photosetWidth = photoset.clientWidth // no padding on this pls :(
+
+        photoset.dataset.photosetLayout.split('').forEach(function (rowLayout) {
+            var row = document.createElement('div')
+            var columnCount = parseInt(rowLayout)
+            var itemWidth = (photosetWidth - Simpleblr.PHOTOSET_SPACING * (columnCount - 1)) / columnCount
+
+            // Calculate row height
+            var rowHeight = Simpleblr.MAX_ROW_HEIGHT
+            for (var columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+                var item = items[itemIndex + columnIndex]
+                var image = item.querySelector('img')
+                var resizedHeight = image.height / image.width * itemWidth
+                if (resizedHeight < rowHeight) {
+                    rowHeight = resizedHeight
+                }
+            }
+
+            // Set item position + dimensions
+            for (var columnIndex = 0; columnIndex < columnCount; columnIndex++) {
+                var item = items[itemIndex + columnIndex]
+                item.dataset.photosetItem = true
+                row.appendChild(item)
+
+                var image = item.querySelector('img')
+                var resizedHeight = image.height / image.width * itemWidth
+                var halfExtraHeight = (resizedHeight - rowHeight) / 2
+                image.width = itemWidth
+                if (halfExtraHeight > 0) {
+                    image.style += "; margin-top: -" + halfExtraHeight + "px"
+                }
+            }
+
+            row.dataset.photosetRow = rowLayout
+            row.style += "; height: " + rowHeight + "px"
+            photoset.appendChild(row)
+        
+            itemIndex += columnCount
+        })
+
+        photoset.dataset.photosetCreated = true
+    })
+}
+
+/* Run everything */
+Simpleblr.run = function () {
+    Simpleblr.loadHighResNPFPhotos()
+    Simpleblr.fixNPFLeadingBreaks()
+    Simpleblr.fixNPFEmptyParagraphs()
+    Simpleblr.layoutPhotosets()
+}
